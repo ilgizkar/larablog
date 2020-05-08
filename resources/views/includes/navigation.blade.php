e5c7e5d16452334a93f6aaf495ab392fb6bf19ce
Ilgiz <karabaev.il@yandex.ru>
2020-05-08 20:13:13 +0300

    FIX
    Signed-off-by: Ilgiz <karabaev.il@yandex.ru>

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a class="navbar-brand" href="{{ route('index') }}">Larablog</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
+            Меню
             <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
